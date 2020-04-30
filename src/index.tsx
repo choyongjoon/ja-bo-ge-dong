19fdbc4da0a9acded6bf8db6146f5de9ec111809
Cho, Yongjoon <w2000wkd@gmail.com>
2020-04-30 23:08:43 +0900

    feat: add react-snap

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
 import "core-js/stable"
 import "regenerator-runtime/runtime"
 import React from "react"
-import ReactDOM, { render } from "react-dom"
+import ReactDOM, { hydrate, render } from "react-dom"
 
 import App from "./App"
+
+const rootElement = document.getElementById("root")
+
+const renderWithSnap = () => {
+  if (rootElement.hasChildNodes()) {
+    hydrate(<App />, rootElement)
+  } else {
+    render(<App />, rootElement)
+  }
+}
 
 if (process.env.NODE_ENV !== "production") {
   import("react-axe").then(({ default: axe }) => {
     axe(React, ReactDOM, 1000)
-    render(<App />, document.getElementById("root"))
+    renderWithSnap()
   })
 } else {
-  render(<App />, document.getElementById("root"))
+  renderWithSnap()
 }
