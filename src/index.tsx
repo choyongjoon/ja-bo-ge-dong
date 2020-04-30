d67bb890dc917ce94a970846e0f5b694a132f8bf
Cho, Yongjoon <w2000wkd@gmail.com>
2020-04-30 22:53:30 +0900

    feat: add react-axe and eslint-plugin-jsx-a11y

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import "core-js/stable"
 import "regenerator-runtime/runtime"
-import * as React from "react"
-import { render } from "react-dom"
+import React from "react"
+import ReactDOM, { render } from "react-dom"
 
 import App from "./App"
 
-render(<App />, document.getElementById("root"))
+if (process.env.NODE_ENV !== "production") {
+  import("react-axe").then(({ default: axe }) => {
+    axe(React, ReactDOM, 1000)
+    render(<App />, document.getElementById("root"))
+  })
+} else {
+  render(<App />, document.getElementById("root"))
+}
