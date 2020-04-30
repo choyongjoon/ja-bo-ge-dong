de43af097864f08b496ae1e1cf3f4bc33fb9f81f
Cho, Yongjoon <w2000wkd@gmail.com>
2020-04-30 21:55:57 +0900

    feat: add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React, { FC } from "react"
 import { hot } from "react-hot-loader/root"
+import styled, { ThemeProvider } from "styled-components"
+
+import theme from "./theme"
 
 const App: FC = () => {
   return (
-    <div>
-      <h1>Base</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Title>Base</Title>
+      </div>
+    </ThemeProvider>
   )
 }
 
 export default hot(App)
+
+const Title = styled.h1`
+  text-align: center;
+  color: ${(props) => props.theme.colors.main};
+`
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components"
+
+const theme: DefaultTheme = {
+  borderRadius: "5px",
+
+  colors: {
+    main: "black",
+    secondary: "magenta",
+  },
+}
+
+export default theme
