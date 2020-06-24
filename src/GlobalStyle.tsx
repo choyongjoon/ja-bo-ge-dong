474089b18e8d112ee65719900c12ca000ea49e86
Cho, Yongjoon <w2000wkd@gmail.com>
2020-06-24 22:05:28 +0900

    feat: add 'keep-all' and default 'font-family'

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -1,6 +1,26 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
+  html {
+    word-break: keep-all;
+  }
+
+  body {
+    font-family:
+      '-apple-system',
+      'system-ui',
+      'BlinkMacSystemFont',
+      'Segoe UI',
+      'Roboto',
+      'Helvetica Neue',
+      'Arial',
+      'sans-serif',
+      'Apple Color Emoji',
+      'Segoe UI Emoji',
+      'Segoe UI Symbol',
+      sans-serif;
+  }
+
   main {
     padding: 0 16px;
   }
